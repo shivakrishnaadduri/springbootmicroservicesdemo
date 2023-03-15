5eed852fd1c40aa19876aef52416b3a5dd50d693
shivakrishna adduri <shivakrishna.github@gmail.com>
2023-03-15 14:37:19 +0530

    Initial commit.

---
+++ microservices-demoapp/src/main/java/com/microservicesapp/api/ms/MicroservicesDemoappApplication.java
@@ -0,0 +1,13 @@
+package com.microservicesapp.api.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDemoappApplication.class, args);
+	}
+
+}
