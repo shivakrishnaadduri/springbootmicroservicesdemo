c49b2997390452758834989e5d0e1343294f75c9
shivakrishna adduri <shivakrishna.github@gmail.com>
2023-03-20 20:07:48 +0530

    Eureka Discovery service.

---
+++ microservices-demoapp-ds/src/test/java/microservicesdemoappds/MicroservicesDemoappDsApplicationTests.java
@@ -0,0 +1,14 @@
+package microservicesdemoappds;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MicroservicesDemoappDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
